7b39b1a8a02e32c2b357eecc115b850eabc162c7
krngrvr09 <krngrvr09@gmail.com>
2015-03-17 03:41:11 +0530

    Changed directory structure to upload on Pypi. Added License.

---
+++ udp_server/setup.py
@@ -0,0 +1,25 @@
+# from distutils.core import setup
+# setup(
+#   name = 'mouse_droid',
+#   packages = ['mouse_droid'], # this must be the same as the name above
+#   version = '0.1',
+#   description = 'mouse droid package',
+#   author = 'Karan Grover',
+#   author_email = 'krngrvr09@gmail.com',
+#   url = 'https://github.com/krngrvr09/MouseDroid', # use the URL to the github repo
+#   download_url = 'https://github.com/krngrvr09/mouse_droid/tarball/0.1', # I'll explain this in a second
+#   keywords = ['mouse', 'android', 'python'], # arbitrary keywords
+#   classifiers = [],
+# )
+
+from distutils.core import setup
+
+setup(name='mouse_droid',
+      version='0.1',
+      description='Control Mouse via pointer',
+      author='Karan Grover',
+      author_email='krngrvr09@gmail.com',
+      url = 'https://github.com/krngrvr09/MouseDroid', # use the URL to the github repo
+      download_url = 'https://github.com/krngrvr09/mouse_droid/tarball/0.1', # I'll explain this in a second
+      keywords = ['mouse', 'android', 'python'], # arbitrary keywords
+     )
