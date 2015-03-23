a79d86794b279af7f1ad65d1ebc71b86f3a2fb08
krngrvr09 <krngrvr09@gmail.com>
2015-03-23 17:26:06 +0530

    attempt to install dependencies automatically along with pip install

--- udp_server/setup.py
+++ udp_server/setup.py
@@ -20,6 +20,10 @@ setup(name='mouse_droid',
       author='Karan Grover',
       author_email='krngrvr09@gmail.com',
       scripts=['mouse_droid/droidserver'],
+      dependency_links = [
+        'https://pypi.python.org/packages/source/n/netifaces/netifaces-0.10.4.tar.gz#md5=36da76e2cfadd24cc7510c2c0012eb1e',
+        'https://pypi.python.org/packages/source/P/PyUserInput/PyUserInput-0.1.9.tar.gz#md5=7a0decf373a67299301afe92c8772b01'
+      ],
       url = 'https://github.com/krngrvr09/MouseDroid', # use the URL to the github repo
       download_url = 'https://github.com/krngrvr09/mouse_droid/tarball/0.1.1', # I'll explain this in a second
       keywords = ['mouse', 'android', 'python'], # arbitrary keywords
