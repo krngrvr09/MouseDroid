37813afc1306fcbbd424e66f0d9209adce1749bd
krngrvr09 <krngrvr09@gmail.com>
2015-03-23 15:14:48 +0530

    attempt to run server as a script

--- udp_server/setup.py
+++ udp_server/setup.py
@@ -15,10 +15,11 @@
 from distutils.core import setup
 
 setup(name='mouse_droid',
-      version='0.1',
+      version='0.1.1',
       description='Control Mouse via pointer',
       author='Karan Grover',
       author_email='krngrvr09@gmail.com',
+      scripts=['mouse_droid/droidserver']
       url = 'https://github.com/krngrvr09/MouseDroid', # use the URL to the github repo
       download_url = 'https://github.com/krngrvr09/mouse_droid/tarball/0.1', # I'll explain this in a second
       keywords = ['mouse', 'android', 'python'], # arbitrary keywords
